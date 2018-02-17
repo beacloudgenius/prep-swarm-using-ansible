b599b738d144052e936045db6f91c0c41f1c9e64
Nilesh <nilesh@cloudgeni.us>
2018-02-16 19:50:07 -0800

    added swarm examples

---
+++ runme.sh
@@ -0,0 +1,7 @@
+ansible-playbook playbooks/0.basics.yaml
+ansible-playbook playbooks/1.packages.yaml
+ansible-playbook playbooks/2.private_ip.yaml
+ansible-playbook playbooks/3.hostfile.yaml
+#ansible-playbook playbooks/4.folders.yaml <- this needs your customization
+ansible-playbook playbooks/5.docker.yaml
+ansible-playbook playbooks/6.swarm.yaml
---
+++ swarm/README.md
@@ -0,0 +1,3 @@
+https://docs.docker.com/engine/swarm/services/
+
+https://docs.traefik.io/user-guide/swarm-mode/#docker-swarm-mode-cluster
